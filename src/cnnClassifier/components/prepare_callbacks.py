a125c8b12b6e3012b9edaeb8c25978c93e062ce5
Suhailakt97 <kt.suhailaabdu@gmail.com>
2025-09-09 20:06:36 +0100

    training added

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -16,7 +16,7 @@ class PrepareCallback:
     def _create_tb_callbacks(self):
         timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
         tb_running_log_dir = os.path.join(
-            self.config.tensorboard_root_log_dir,
+            self.config.tensorboard_log_dir,
             f"tb_logs_at_{timestamp}",
         )
         return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
